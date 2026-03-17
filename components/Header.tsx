9301aea843826d853c3eb70a22abfdcc176b13d6
vinikyo <vinicius_garcia@outlook.com.br>
2026-03-17 16:51:06 -0300

    feat: initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Header() {
+    return <header className="min-h-[10vh] px-16 py-6 w-full bg-white flex justify-between items-center shadow-sm"></header>
+}
+
+export default Header;
