78985c85838f0312d754fac0e7cb99cf2b46aa5f
vinikyo <vinicius_garcia@outlook.com.br>
2026-03-27 18:19:26 -0300

    pages funcionais

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,61 @@
-import React from 'react'
+"use client";
+
+import { Bookmark, Heart, LayoutDashboard } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 function Header() {
-    return <header className="min-h-[10vh] px-16 py-6 w-full bg-white flex justify-between items-center shadow-sm"></header>
+const pathname = usePathname();
+    const menu = [
+        {
+            name: "Browse",
+            link: "/",
+            icon: <LayoutDashboard size={20}/>,
+
+        },
+                {
+            name: "Favorites",
+            link: "/favourites",
+            icon: <Heart size={22}/>,
+
+        },
+                {
+            name: "Saved",
+            link: "/bookmarks",
+            icon: <Bookmark size={22}/>,
+
+        },
+
+    ];
+    return ( <header className="min-h-[10vh] px-16 py-6 w-full bg-white flex justify-between items-center shadow-sm">
+        <Link href='/'>
+        <Image src={"/International_Pokémon_logo.svg"} width={120} height={90} alt="logo" />
+        </Link>
+        
+    <nav>
+        <ul className="flex items-center gap-8 text-gray-400">
+            {menu.map((item, index: number) => (
+                <li key={index}>
+                    <Link href={item.link} className={`py-2 px-6 text-sm flex items-center gap-2 font-bold rounded-lg
+                            ${
+                                pathname === item.link ? 'bg-[#6c5ce7]/15 text-[#6c5ce7]' :
+                                ''
+                            }
+                        `}>
+
+
+                    <span>{item.icon}</span>
+                    <span>{item.name}</span>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    </nav>
+    <div></div>
+    </header>
+    );
 }
 
 export default Header;
